dce49806ea7cb6016a5408489c4c4fe818f1e99d
kartavya-p <kartavya.98p@gmail.com>
2025-12-17 14:08:12 +0530

    auth provider

---
+++ src/main/java/com/eazybank/config/EazyBankAuthenticationProvder.java
@@ -0,0 +1,37 @@
+package com.eazybank.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EazyBankAuthenticationProvder implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+
+        UserDetails user = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(pwd, user.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, pwd, user.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Invalid Password!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
